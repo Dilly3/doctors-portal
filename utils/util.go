4b5d779596fb5bf6f5ba13bf30bcbf2f047cdb75
michael anikamadu <dil.anikamadu@gmail.com>
2022-08-31 23:29:18 +0100

    utils/

---
+++ utils/util.go
@@ -0,0 +1,51 @@
+package utils
+
+import (
+	"crypto/rand"
+	"fmt"
+	"log"
+	"os"
+)
+
+type ErrorResponse struct {
+	Code    int         `json:"code"`
+	Error   string      `json:"error"`
+	Message interface{} `json:"message"`
+}
+type NoAppointment struct {
+	Message string `json:"message"`
+	Name    string `json:"name"`
+}
+type SuccessResponse struct {
+	Code    int         `json:"code"`
+	Object  interface{} `json:"object"`
+	Message interface{} `json:"message"`
+}
+
+func GetPortFromEnv() string {
+	port := os.Getenv("ROUTER_PORT")
+	if port == "" || len(port) < 1 {
+		port = "8080"
+	}
+	return port
+}
+func GenerateRandomID() string {
+	b := make([]byte, 9)
+
+	_, err := rand.Read(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return fmt.Sprintf("%s%x", "HTH-", b)
+}
+func GenerateRandomAppointmentID() string {
+	b := make([]byte, 5)
+
+	_, err := rand.Read(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return fmt.Sprintf("%s%x", "HTH-", b)
+}
